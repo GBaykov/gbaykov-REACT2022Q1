4852eab9a902fb2bbc207579115b40c55ad2a328
GBaykov <gleb.bykov99@gmail.com>
2022-03-27 03:12:14 +0300

    feat: add simple basic card

--- components/src/components/card/card.tsx
+++ components/src/components/card/card.tsx
@@ -1,3 +1,21 @@
 import React, { Component } from 'react';
+import './card.css';
 
-export default class Сard extends Component {}
+export default class Сard extends Component {
+  cardProperty = {
+    name: 'Jon',
+    password: 'Gon1234534sdf',
+    age: '18',
+    date: '01/01/0101 ',
+  };
+  render() {
+    return (
+      <section className="card">
+        <p>{this.cardProperty.name}</p>
+        <p>{this.cardProperty.password}</p>
+        <p>{this.cardProperty.age}</p>
+        <p>{this.cardProperty.date}</p>
+      </section>
+    );
+  }
+}
--- components/src/index.tsx
+++ components/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from './components/card';
 import Input from './components/input';
 import './index.css';
-
-ReactDOM.render(<Input />, document.getElementById('root'));
+ReactDOM.render(<Card />, document.getElementById('root'));
