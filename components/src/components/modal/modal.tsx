6ebfc7b530afaee75db38c6ccc3290772fde5c0a
GBaykov <gleb.baykov99@gmail.com>
2022-04-16 01:20:00 +0300

    style: add more styles for c=modal wind

--- components/src/components/modal/modal.tsx
+++ components/src/components/modal/modal.tsx
@@ -10,12 +10,17 @@ export default class Modal extends Component<IModalProps> {
       <>
         <div className="overlay" />
         <div className="modal">
+          <p className="modal-cross" />
+          <img src={character.image} alt="image of character" className="card-img" />
           <div className="modal-content">
-            <p className="modal-cross" />
-            <img src={character.image} alt="image of character" className="card-img" />
-            <p className="modal-item">{`name: ${character.name}`}</p>
             <p className="modal-item">{`gender: ${character.gender}`}</p>
+            <p className="modal-item">{`name: ${character.name}`}</p>
             <p className="modal-item">{`status: ${character.status}`}</p>
+            <p className="modal-item">{`episode: ${character.episode}`}</p>
+            <p className="modal-item">{`species: ${character.species}`}</p>
+            <p className="modal-item">{`origin: ${character.origin.name}`}</p>
+            <p className="modal-item">{`location: ${character.location.name}`}</p>
+            <p className="modal-item">{`location: ${character.location.url}`}</p>
           </div>
         </div>
       </>
