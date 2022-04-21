6efb7d66487a493af935bd2938c324292b63ae49
GBaykov <gleb.baykov99@gmail.com>
2022-04-22 00:35:55 +0300

    refactor: rewrite modalWindow, errorMessage, app components on FC

--- components/src/components/App/app.tsx
+++ components/src/components/App/app.tsx
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from '../header';
 import { BrowserRouter } from 'react-router-dom';
-import Api from '../../services/api';
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    );
-  }
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
+  );
 }
--- components/src/components/modal/modal.tsx
+++ components/src/components/modal/modal.tsx
@@ -2,31 +2,29 @@ import React, { Component } from 'react';
 import { IModalProps } from '../../types/types';
 import './modal.css';
 
-export default class Modal extends Component<IModalProps> {
-  onModalClose = () => {
-    this.props.closeOpenModal(false);
+export default function Modal({ character, closeOpenModal }: IModalProps) {
+  const onModalClose = () => {
+    closeOpenModal(false);
   };
-  render() {
-    const { character } = this.props;
-    if (!character) return null;
-    return (
-      <>
-        <div className="overlay" onClick={this.onModalClose} />
-        <div className="modal">
-          <p className="modal-cross" onClick={this.onModalClose} />
-          <img src={character.image} alt="image of character" className="card-img" />
-          <div className="modal-content">
-            <p className="modal-item">{`gender: ${character.gender}`}</p>
-            <p className="modal-item">{`name: ${character.name}`}</p>
-            <p className="modal-item">{`status: ${character.status}`}</p>
-            <p className="modal-item">{`episode: ${character.episode}`}</p>
-            <p className="modal-item">{`species: ${character.species}`}</p>
-            <p className="modal-item">{`origin: ${character.origin.name}`}</p>
-            <p className="modal-item">{`location: ${character.location.name}`}</p>
-            <p className="modal-item">{`location: ${character.location.url}`}</p>
-          </div>
+
+  if (!character) return null;
+  return (
+    <>
+      <div className="overlay" onClick={onModalClose} />
+      <div className="modal">
+        <p className="modal-cross" onClick={onModalClose} />
+        <img src={character.image} alt="image of character" className="card-img" />
+        <div className="modal-content">
+          <p className="modal-item">{`gender: ${character.gender}`}</p>
+          <p className="modal-item">{`name: ${character.name}`}</p>
+          <p className="modal-item">{`status: ${character.status}`}</p>
+          <p className="modal-item">{`episode: ${character.episode}`}</p>
+          <p className="modal-item">{`species: ${character.species}`}</p>
+          <p className="modal-item">{`origin: ${character.origin.name}`}</p>
+          <p className="modal-item">{`location: ${character.location.name}`}</p>
+          <p className="modal-item">{`location: ${character.location.url}`}</p>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
