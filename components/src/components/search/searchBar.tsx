8e4caed7d222f8f05fa0853505e017d55490fd05
GBaykov <gleb.baykov99@gmail.com>
2022-04-26 00:18:08 +0300

    feat: implement useEffectin searchBar

--- components/src/components/search/searchBar.tsx
+++ components/src/components/search/searchBar.tsx
@@ -1,58 +1,66 @@
-import React, { Component } from 'react';
-import { SearchBarProps, SearchBarState } from '../../types/types';
+import React, { useState, useEffect } from 'react';
+import { SearchBarProps } from '../../types/types';
 import './search.css';
 
-export default class SearchBar extends Component<SearchBarProps, SearchBarState> {
-  state = {
-    inputValue: '',
-  };
-  constructor(props: SearchBarProps) {
-    super(props);
+export default function SearchBar(props: SearchBarProps) {
+  const [inputValue, setInputValue] = useState<string>('');
 
-    this.componentCleanup = this.componentCleanup.bind(this);
+  useEffect(() => {
+    const inputValueLS = localStorage.getItem('inputValue');
+    if (!!inputValueLS) setInputValue(inputValueLS);
+    window.addEventListener('beforeunload', componentCleanup);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      componentCleanup();
+      window.removeEventListener('beforeunload', componentCleanup);
+    };
+  });
+
+  // constructor(props: SearchBarProps) {
+  //   super(props);
+
+  //   this.componentCleanup = this.componentCleanup.bind(this);
+  // }
+  const onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value);
+  };
+
+  function componentCleanup() {
+    localStorage.setItem('inputValue', inputValue);
   }
-  onInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({
-      inputValue: e.currentTarget.value,
-    });
-  };
 
-  componentCleanup() {
-    localStorage.setItem('inputValue', this.state.inputValue);
-  }
+  // componentDidMount = () => {
+  //   const inputValue = localStorage.getItem('inputValue');
+  //   this.setState({
+  //     inputValue,
+  //   });
+  //   window.addEventListener('beforeunload', this.componentCleanup);
+  // };
 
-  componentDidMount = () => {
-    const inputValue = localStorage.getItem('inputValue');
-    this.setState({
-      inputValue,
-    });
-    window.addEventListener('beforeunload', this.componentCleanup);
-  };
+  // componentWillUnmount = () => {
+  //   this.componentCleanup();
+  //   window.removeEventListener('beforeunload', this.componentCleanup);
+  // };
 
-  componentWillUnmount = () => {
-    this.componentCleanup();
-    window.removeEventListener('beforeunload', this.componentCleanup);
-  };
-
-  handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    this.props.onSearchSubmit(this.state.inputValue);
+    props.onSearchSubmit(inputValue);
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          <input
-            className="searchbar"
-            placeholder="Search area"
-            type="text"
-            value={this.state.inputValue}
-            onChange={this.onInputChange}
-          />
-        </label>
-        <input type="submit" value="Отправить" />
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          className="searchbar"
+          placeholder="Search area"
+          type="text"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+      </label>
+      <input type="submit" value="Отправить" />
+    </form>
+  );
 }
